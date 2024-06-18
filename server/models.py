319e923293242c744ed82b45255e66a1e73427be
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-06-18 12:39:24 -0400

    adds message model

--- server/models.py
+++ server/models.py
@@ -12,3 +12,8 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    username = db.Column(db.String)
+    created_at =  db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
